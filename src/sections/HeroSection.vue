6d08c00064fa2f4df3d47a1cb2e7a318bf1908ea
freddysae0 <freddysae00@gmail.com>
2024-04-15 17:39:03 -0400

    Renaming components to sections

---
+++ src/sections/HeroSection.vue
@@ -0,0 +1,22 @@
+<template>
+  <section id="hero" class="min-h-screen flex">
+    <div class="w-1/2 flex justify-center items-center">
+      <InteractionHero></InteractionHero>
+    </div>
+    <div class="w-1/2 flex justify-center items-center">div</div>
+  </section>
+</template>
+
+<script>
+import InteractionHero from "@/components/hero/InteractionHero.vue";
+export default {
+  data: () => {
+    return {};
+  },
+  components: {
+    InteractionHero,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
