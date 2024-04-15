94e241d1981bc1621e6c9504def9318ac845c953
freddysae0 <freddysae00@gmail.com>
2024-04-15 17:35:24 -0400

    Adding Apercu Pro font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,18 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   content: [],
   theme: {
-    extend: {},
-    colors: {
-      white: "#ffffff",
-      primary: "#685DC5",
-      secondary: "#4fa16c",
-      text: {
-        200: "#6A6A6A",
-        100: "#535353",
+    extend: {
+      colors: {
+        white: "#ffffff",
+        primary: "#685DC5",
+        secondary: "#4fa16c",
+        text: {
+          200: "#6A6A6A",
+          100: "#535353",
+        },
+      },
+      fontFamily: {
+        apercu: ["Apercu Pro"],
       },
     },
   },
