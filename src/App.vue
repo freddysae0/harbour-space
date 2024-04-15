61d958973fa65aa97cf354102319791dcc8e342c
freddysae0 <freddysae00@gmail.com>
2024-04-15 17:38:30 -0400

    renaming components to sections for better understanding, adding global colors and classes.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <NavbarComponent></NavbarComponent>
-  <FooterComponent class="fixed bottom-0 w-full"></FooterComponent>
-  <main></main>
+  <NavbarSection></NavbarSection>
+  <main><HeroSection></HeroSection></main>
+  <FooterSection class="fixed bottom-0 w-full"></FooterSection>
 </template>
 
 <script>
-import NavbarComponent from "./components/NavbarComponent.vue";
-import FooterComponent from "./components/FooterComponent.vue";
+import NavbarSection from "./sections/NavbarSection.vue";
+import FooterSection from "./sections/FooterSection.vue";
+import HeroSection from "./sections/HeroSection.vue";
 export default {
   name: "App",
-  components: { NavbarComponent, FooterComponent },
+  components: { NavbarSection, FooterSection, HeroSection },
 };
 </script>
 
@@ -19,7 +20,23 @@ export default {
 
   --primary: #685dc5;
   --secondary: #4fa16c;
-  --text-2: #6a6a6a;
   --text-1: #535353;
+  --text-2: #6a6a6a;
+  --border-1: #dadada;
+}
+.title-section {
+  /* MA Interaction Desig */
+
+  font-style: normal;
+  font-size: 45px;
+  line-height: 56px;
+  /* or 117% */
+  letter-spacing: -0.6px;
+
+  color: var(--primary);
+}
+.card {
+  border: 1px solid var(--border-1);
+  border-radius: 8px;
 }
 </style>
